d7c73969615d11c4affc4b04ff24bf912b7e1bf9
Jenna Rowan <jennagirl001@gmail.com>
2021-07-12 15:24:05 -0400

    Extended map marker popups to list all useful volcano info

--- main.py
+++ main.py
@@ -13,8 +13,15 @@ feature_group_one = folium.FeatureGroup(name = "My Map")
 # Creates the map markers for all the volcanoes in the data file, adds them to map
 for index, volcano in volcanoes.iterrows():
 
-    feature_group_one.add_child(folium.Marker([volcano["LAT"], volcano["LON"]], tooltip = volcano["NAME"], popup = volcano["LOCATION"], icon = folium.Icon(color = "purple")))
+    # Creates the basic data set for each volcano that will populate in the popup when the volcano's map marker is clicked
+    iframe = folium.IFrame("Name: " + volcano["NAME"] + "<br>" + "Location: " + volcano["LOCATION"][3:] + "<br>" + "Status: " + volcano["STATUS"] + "<br>" + "Elevation: " + str(volcano["ELEV"]) + "<br>" + "Type: " + volcano["TYPE"])
 
+    popup = folium.Popup(iframe, min_width=250, max_width=250)
+
+    # Final marker creaton
+    feature_group_one.add_child(folium.Marker([volcano["LAT"], volcano["LON"]], tooltip = volcano["NAME"], popup = popup, icon = folium.Icon(color = "purple")))
+
+# Adds all markers to the map
 map.add_child(feature_group_one)
 
 # Creates/Overwrites map file
