0ba16d8f740462cf26d7eb1915c42446903f13a4
Jenna Rowan <jennagirl001@gmail.com>
2021-07-11 21:29:45 -0400

    +initial map marker code to tag all US volcanoes

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import folium
+import pandas
+
+# Loads in data file containing all info on volcanoes as a data frame
+volcanoes = pandas.read_csv("Volcanoes.txt")
 
 # Creates map centered on US
 map = folium.Map([38, -99], zoom_start = 5, tiles = "Stamen Terrain")
-map.add_child(folium.Marker([38, -99], tooltip="Yo homie", popup = "Look at me", icon = folium.Icon(color = "green")))
+
+# Housekeeping
+feature_group_one = folium.FeatureGroup(name = "My Map")
+
+# Creates the map markers for all the volcanoes in the data file, adds them to map
+for index, volcano in volcanoes.iterrows():
+
+    feature_group_one.add_child(folium.Marker([volcano["LAT"], volcano["LON"]], tooltip = volcano["NAME"], popup = volcano["LOCATION"], icon = folium.Icon(color = "purple")))
+
+map.add_child(feature_group_one)
+
+# Creates/Overwrites map file
 map.save("Map1.html")
